18617dccc95629fba575f9212093871bc5c7ac5d
yayegeyay <ege.yay@mail.mcgill.ca>
2024-04-16 17:20:22 -0400

    Update gtag.js

--- assets/gtag.js
+++ assets/gtag.js
@@ -1,9 +1,8 @@
 <!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-8PVVY2GYRH"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
+<><script async src="https://www.googletagmanager.com/gtag/js?id=G-8PVVY2GYRH"></script><script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments)};}
+    gtag('js', new Date());
 
-  gtag('config', 'G-8PVVY2GYRH');
-</script>
+    gtag('config', 'G-8PVVY2GYRH');
+</script></>
